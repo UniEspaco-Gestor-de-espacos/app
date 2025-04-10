a3855daf45df9dae742174f2cf4c2887db62601b
phplemos <phplemos.dev@gmail.com>
2025-04-10 14:26:46 -0300

    feat: Adicionando migrations e alterando o banco para postgres

---
+++ database/migrations/2025_04_10_165731_create_horario_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('horario', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('agendaId')->constrained('agenda_turno');
+            $table->time('horarioInicio');
+            $table->time('horarioFim');
+            $table->date('data');
+            $table->unique(['agendaId', 'horarioInicio', 'data']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('horario');
+    }
+};
