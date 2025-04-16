b0df97acac3f2d9a16df03eda61d86461196f621
phplemos <phplemos.dev@gmail.com>
2025-04-16 17:25:07 -0300

    chore: fazendo relacionamento entre Setor e Usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Setor;
 
 class User extends Authenticatable
 {
@@ -23,6 +24,7 @@ class User extends Authenticatable
         'password',
         'telefone',
         'profile_pic',
+        'setor_id' // testes
     ];
 
     /**
@@ -46,5 +48,8 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function setors(){
+        return $this->hasOne(Setor::class);
     }
 }
